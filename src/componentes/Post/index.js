13b2434d2ef917305759b8fb804ff644ad71f79d
Panagiotis Alexandro Tsilfidis <105652220+tsilfidis@users.noreply.github.com>
2024-06-19 16:54:45 -0400

    Aula 4
    Identificar quando utilizar rotas dinâmicas;
    
    Rotas dinâmicas são úteis quando queremos ter uma mesma estrutura de página e mudar seu conteúdo de acordo com alguma informação passada na URL. No nosso caso, passamos o id de um post pela URL, buscamos o post de acordo com o id e mostramos suas informações na tela.
    Utilizar os seletores de rotas dinâmicas para criar uma rota dinâmica;
    
    Utilizamos a sintaxe /caminho/:nomedoparametro no atributo path de uma Route.
    Utilizar o hook useParams.
    
    Com ele, obtemos os parâmetros passados para uma rota dinâmica.

--- src/componentes/Post/index.js
+++ src/componentes/Post/index.js
@@ -1,16 +1,31 @@
-import styles from "./Post.module.css";
+import './Post.css'
+;
+import { useParams } from "react-router-dom";
+import posts from '../../json/posts.json';
+import PostModelo from '../../componentes/PostModelo';
+import ReactMarkdown from "react-markdown";
 
-export default function Post( { post} ) {
+export default function Post() {
+    const parametros = useParams();
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
+
+    if (!post) {
+        return <h1>Post não encontrado...</h1>
+    }
+
     return (
-        <div className={styles.post}>
-            <img 
-                className={styles.capa}
-                src={`../../assets/posts/${post.id}/capa.png`}
-                alt="Imagem de capa do post"
-            />
-            <h2 className={styles.titulo}>{post.titulo}</h2>
-
-            <button className={styles.botaoLer}>Ler</button>
-        </div>
+        <PostModelo 
+            fotoCapa={`../../assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+        </PostModelo>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ import SobreMim from "./paginas/SobreMim"
 import Menu from "./componentes/Menu";
 import Rodape from "./componentes/Rodape";
 import PaginaPadrao from "./componentes/PaginaPadrao";
-
+import Post from "./componentes/Post";
+import NaoEncontrada from "./paginas/NaoEncontrada";
 
 function AppRoutes() {
   return (
@@ -15,9 +16,10 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio/>} />
           <Route path="/sobremim" element={<SobreMim/>} />
+          <Route path="posts/:id" element={<Post />}/>
         </Route>
 
-        <Route path="*" element={<div>Página não encontrada!</div>} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
